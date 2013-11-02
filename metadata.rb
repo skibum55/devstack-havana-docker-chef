4249205b7db18adc33ad25a01ec0c11b6c538c81
Sean Keery <skibum55@hotmail.com>
2013-11-01 22:51:50 -0600

    Update metadata.rb
    docs

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email "skibum55@homail.com"
 license          "Apache 2.0"
 description      "A cookbook for creating a devstack & docker environment."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.4"
+recipe            "devstack-havana-docker-chef", "Installs/Configures Devstack with Docker"
+recipe            "devstack-havana-docker-chef::_install_docker", "Installs/configures Docker registy container"
 
 %w[
   ubuntu
